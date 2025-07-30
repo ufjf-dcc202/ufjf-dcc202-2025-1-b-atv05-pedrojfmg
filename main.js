08988fbdf78dc76a0c3a41aca6be1930cae44f85
pedrojfmg <pedropandolfo10@gmail.com>
2025-07-30 17:01:47 -0300

    adiciona cores para discos

--- main.js
+++ main.js
@@ -4,16 +4,26 @@
     const eTabuleiro = criaTabuleiro();
     document.body.append(eTabuleiro);
     
+    for(let i=0; i < 7; i++){
+        const eDisco = criaDisco();
+        eTabuleiro.append(eDisco);
+
+        if(i < 3){
+            eDisco.dataset.cor = "branco";
+        } else if (i > 3){
+            eDisco.dataset.cor = "preto";
+        } else {
+            eDisco.dataset.cor = "transparente";
+        }
+    }
+    
     function criaDisco(){
         const novoDisco = document.createElement("Div");
         novoDisco.classList.add("disco");
         return novoDisco;
     }
 
-    for(let i=0; i < 7; i++){
-        const eDisco = criaDisco();
-        eTabuleiro.append(eDisco);
-    }
+    
 
     function criaTabuleiro(){
         const novoTabuleiro = document.createElement("Div");
