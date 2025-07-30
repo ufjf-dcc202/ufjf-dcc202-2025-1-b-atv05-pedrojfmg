df85b9d0be98398688782469ccc9753552be0995
pedrojfmg <pedropandolfo10@gmail.com>
2025-07-30 16:35:59 -0300

    adiciona o "cria tabuleiro"

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
     const eH1 = document.querySelector("h1");
     eH1.textContent = "Ola Mundo!";
+
+    const eTabuleiro = criaTabuleiro();
+    document.body.append(eTabuleiro);
     
+
+    function criaTabuleiro(){
+        const novoTabuleiro = document.createElement("Div");
+        novoTabuleiro.classList.add("Tabuleiro");
+        novoTabuleiro.style.backgroundColor = "lightgreen";
+        novoTabuleiro.style.minHeight = "25px";
+        return novoTabuleiro;
+    }
