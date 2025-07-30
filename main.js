08754be482ed25f8b8efbe0849424bf9d032a125
pedrojfmg <pedropandolfo10@gmail.com>
2025-07-30 16:48:09 -0300

    adiciona o "cria discos"

--- main.js
+++ main.js
@@ -4,9 +4,24 @@
     const eTabuleiro = criaTabuleiro();
     document.body.append(eTabuleiro);
     
+    function criaDisco(){
+        const novoDisco = document.createElement("Div");
+        novoDisco.classList.add("disco");
+        return novoDisco;
+    }
+
+    for(let i=0; i < 7; i++){
+        const eDisco = criaDisco();
+        eTabuleiro.append(eDisco);
+    }
 
     function criaTabuleiro(){
         const novoTabuleiro = document.createElement("Div");
         novoTabuleiro.classList.add("tabuleiro");
         return novoTabuleiro;
     }
+
+    
+
+    
+    
